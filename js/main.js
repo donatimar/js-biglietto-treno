4e3a8a7312732b28cde0b65cabd368b2935cb9a0
donatimar <donatimar@gmail.com>
2024-10-14 15:36:39 +0200

    added raccolta dati

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // # TRACCIA
 
-/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:- il prezzo del biglietto è definito in base ai km (0.21 € al km)
 - va applicato uno sconto del 20% per i minorenni
 - va applicato uno sconto del 40% per gli over 65.
 - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
@@ -9,6 +9,14 @@
 // # PROCEDURA
 
 // ! RACCOLTA DATI
+
+// Chiedo all'utente il numero di chilometri che vuole percorrere
+let km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+console.log(km);
+
+// Chiedo all'utente l'età del passeggero
+let age = parseInt(prompt("Qual è l'età del passeggero?"));
+console.log(age);
 
 // ! ELABORAZIONE DATI
 

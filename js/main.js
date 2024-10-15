c4a1a89fcf1bf1294ca419e1d2f06bbfd5b7c1b6
donatimar <donatimar@gmail.com>
2024-10-15 10:41:40 +0200

    small fixes comments

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// # TRACCIA
+// * TRACCIA
 
 /* 
 Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:- il prezzo del biglietto è definito in base ai km (0.21 € al km)
@@ -7,7 +7,7 @@ Il programma dovrà chiedere all'utente il numero di chilometri che vuole percor
 - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 */
 
-// # PROCEDURA
+// * PROCEDURA
 
 // ! RACCOLTA DATI
 
